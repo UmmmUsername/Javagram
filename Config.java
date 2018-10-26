073c22997c9322798668da29c843869f715a7a47
Victor Kozlenko <kozzlenko@yandex.ru>
2018-10-26 14:40:49 +0700

    Update Config.java

--- Config.java
+++ Config.java
@@ -1,5 +1,5 @@
 public class Config {
-    public static final int APP_ID = 126401;
-    public static final String SERVER = "149.154.167.50:443";
-    public static final String APP_HASH = "77ae105a6367adbfd1d384e5fa710a62";
+    public static final int APP_ID = 000000; // Sample app id
+    public static final String SERVER = "171.0.0.1:443"; // Sample IP
+    public static final String APP_HASH = "ae105a636777adb710a62fd1d384e5fa"; // Sample hash
 }
